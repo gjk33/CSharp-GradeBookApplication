bf8a15caab432b6326156d136e82eba949b22e1d
Gregory Kelly <gxk848@student.bham.ac.uk>
2019-07-07 16:28:22 +0100

    Overwridden Calculate Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,9 +6,8 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook: BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
@@ -22,14 +21,13 @@ namespace GradeBook.GradeBooks
             }
             else
             {
-
                 //This number corresponds to 20% of students 
                 var threshold = (int) Math.Ceiling(Students.Count * 0.2);
-                
+
                 //We then have a sorted list of average grades 
                 var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-                
-                
+
+
                 //We then check if the grade inputted is more than or equal to the grade that is occupying the 80% percentile point 
                 if (grades[threshold - 1] <= averageGrade)
                 {
@@ -58,12 +56,24 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine(
+                    "Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
-            else{
-                base.CalculateStatistics();
+
+            base.CalculateStatistics();
+        }
+
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine(
+                        "Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
