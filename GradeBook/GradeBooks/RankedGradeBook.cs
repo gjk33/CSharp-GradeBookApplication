74aa97177765a2fb9b35f3a9cea79291d9ba96f3
Gregory Kelly <gxk848@student.bham.ac.uk>
2019-07-07 16:03:18 +0100

    Added letter grade override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,62 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook: BaseGradeBook
     {
+        
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            else
+            {
+
+                //This number corresponds to 20% of students 
+                var threshold = (int) Math.Ceiling(Students.Count * 0.2);
+                
+                //We then have a sorted list of average grades 
+                var grades = Students.OrderBy(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+                
+                
+                //We then check if the grade inputted is more than or equal to the grade that is occupying the 80% percentile point 
+                if (grades[threshold - 1] <= averageGrade)
+                {
+                    return 'A';
+                }
+                else if (grades[(threshold * 2) - 1] <= averageGrade)
+                {
+                    return 'B';
+                }
+                else if (grades[(threshold * 3) - 1] <= averageGrade)
+                {
+                    return 'C';
+                }
+                else if (grades[(threshold * 4) - 1] <= averageGrade)
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'F';
+                }
+                
+
+
+
+            }
+        }
+
     }
 }
